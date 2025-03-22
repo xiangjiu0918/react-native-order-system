fd97c2f2414831efb9fda5e5b850be75ff197630
xiangjiu0918 <2216236428@qq.com>
2025-03-22 11:14:28 +0800

    修改keyprfix同名Bug

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,6 +1,7 @@
 const {
   RateLimiterCluster,
   BurstyRateLimiter,
+  // RateLimiterMemory,
 } = require("rate-limiter-flexible");
 const numCPUs = require("node:os").cpus().length;
 const { failure } = require("../utils/responses");
@@ -8,13 +9,13 @@ const { TooManyRequests } = require("http-errors");
 
 const rateLimiter = new BurstyRateLimiter(
   new RateLimiterCluster({
-    keyPrefix: "myclusterlimiter", // Must be unique for each limiter
+    keyPrefix: "commonlimiter", // Must be unique for each limiter
     points: numCPUs * 5,
     duration: 1,
   }),
   new RateLimiterCluster({
-    keyPrefix: "myclusterlimiter", // Must be unique for each limiter
-    points: numCPUs * 45,
+    keyPrefix: "outBrustlimiter", // Must be unique for each limiter
+    points: numCPUs * 60,
     duration: 60,
   })
 );
